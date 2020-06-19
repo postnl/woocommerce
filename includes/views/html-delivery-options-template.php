8554be1141fdefe5373b9a73f6f8310713a55e83
Adem Demir <adem@myparcel.nl>
2020-06-19 12:55:46 +0200

    Update html-delivery-options-template.php

--- includes/views/html-delivery-options-template.php
+++ includes/views/html-delivery-options-template.php
@@ -1,11 +1,11 @@
-<div class="woocommerce-myparcel__delivery-options">
+<div class="woocommerce-postnl__delivery-options">
     <?php
     // Add custom css to the delivery options, if any
-    if (!empty(WCMP()->setting_collection->getByName(WCMP_Settings::SETTING_DELIVERY_OPTIONS_CUSTOM_CSS))) {
+    if (!empty(wCPN()->setting_collection->getByName(wCPN_Settings::SETTING_DELIVERY_OPTIONS_CUSTOM_CSS))) {
         echo "<style>";
-        echo WCMP()->setting_collection->getByName(WCMP_Settings::SETTING_DELIVERY_OPTIONS_CUSTOM_CSS);
+        echo wCPN()->setting_collection->getByName(wCPN_Settings::SETTING_DELIVERY_OPTIONS_CUSTOM_CSS);
         echo "</style>";
     }
     ?>
-  <div id="myparcel-delivery-options"></div>
+  <div id="postnl-delivery-options"></div>
 </div>
