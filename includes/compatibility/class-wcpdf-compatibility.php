753ab1928a652642dfd7c63aae1d95ccde200114
Adem Demir <adem@myparcel.nl>
2020-06-19 09:28:52 +0200

    Update class-wcpdf-compatibility.php

--- includes/compatibility/class-wcpdf-compatibility.php
+++ includes/compatibility/class-wcpdf-compatibility.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace WPO\WC\MyParcel\Compatibility;
+namespace WPO\WC\PostNL\Compatibility;
 
 use Exception;
-use WCMP_Frontend;
-use WPO\WC\MyParcel\Compatibility\Order as WCX_Order;
+use WCPN_Frontend;
+use WPO\WC\PostNL\Compatibility\Order as WCX_Order;
 
 /**
  * Class for compatibility with the WooCommerce PDF Invoices & Packing Slips Premium Templates plugin.
@@ -17,14 +17,14 @@ class WCMP_WCPDF_Compatibility
     {
         // WooCommerce PDF Invoices & Packing Slips Premium Templates compatibility
         add_filter(
-            "wpo_wcpdf_templates_replace_myparcel_track_trace",
+            "wpo_wcpdf_templates_replace_postnl_track_trace",
             ["WCMP_WCPDF_Compatibility", "track_trace"],
             10,
             2
         );
 
         add_filter(
-            "wpo_wcpdf_templates_replace_myparcel_track_trace_link",
+            "wpo_wcpdf_templates_replace_postnl_track_trace_link",
             ["WCMP_WCPDF_Compatibility", "track_trace_link"],
             10,
             2
@@ -40,7 +40,7 @@ class WCMP_WCPDF_Compatibility
      */
     public function track_trace($replacement, $order)
     {
-        $shipments = WCMP_Frontend::getTrackTraceShipments(WCX_Order::get_id($order));
+        $shipments = WCPN_Frontend::getTrackTraceShipments(WCX_Order::get_id($order));
 
         $track_trace = [];
 
@@ -62,7 +62,7 @@ class WCMP_WCPDF_Compatibility
      */
     public function track_trace_link($replacement, $order)
     {
-        $track_trace_links = WCMP_Frontend::getTrackTraceShipments(WCX_Order::get_id($order));
+        $track_trace_links = WCPN_Frontend::getTrackTraceShipments(WCX_Order::get_id($order));
 
         $track_trace_links = array_map(
             function ($link) {
