16716467e2c426a1650380d0c61be469db6fa1e5
Adem Demir <adem@myparcel.nl>
2020-06-22 22:44:52 +0200

    Update class-wcpn-log.php

--- includes/class-wcpn-log.php
+++ includes/class-wcpn-log.php
@@ -4,11 +4,11 @@ if (! defined('ABSPATH')) {
     exit;
 } // Exit if accessed directly
 
-if (class_exists('WCMP_Log')) {
+if (class_exists('WCPN_Log')) {
     return;
 }
 
-class WCMP_Log
+class WCPN_Log
 {
 
     /**
@@ -18,7 +18,7 @@ class WCMP_Log
      */
     public static function add(string ...$messages): void
     {
-        if (! WCMP()->setting_collection->isEnabled(WCMP_Settings::SETTING_ERROR_LOGGING)) {
+        if (! WCPN()->setting_collection->isEnabled(WCPN_Settings::SETTING_ERROR_LOGGING)) {
             return;
         }
 
@@ -27,7 +27,7 @@ class WCMP_Log
         // Starting with WooCommerce 2.7, logging can be grouped by context and severity.
         if (class_exists("WC_Logger") && version_compare(WOOCOMMERCE_VERSION, "2.7", ">=")) {
             try {
-                (wc_get_logger())->debug($message, ["source" => "wc-myparcel"]);
+                (wc_get_logger())->debug($message, ["source" => "wc-postnl"]);
             } catch (Exception $e) {
                 exit($e);
             }
@@ -36,7 +36,7 @@ class WCMP_Log
 
         if (class_exists("WC_Logger")) {
             $wc_logger = function_exists("wc_get_logger") ? wc_get_logger() : new WC_Logger();
-            $wc_logger->add("wc-myparcel", $message);
+            $wc_logger->add("wc-postnl", $message);
 
             return;
         }
@@ -45,7 +45,7 @@ class WCMP_Log
         // add file in upload folder - wp-content/uploads
         $upload_dir        = wp_upload_dir();
         $upload_base       = trailingslashit($upload_dir["basedir"]);
-        $log_file          = $upload_base . "myparcel_log.txt";
+        $log_file          = $upload_base . "postnl_log.txt";
         $current_date_time = date("Y-m-d H:i:s");
         $message           = $current_date_time . " " . $message . "n";
         file_put_contents($log_file, $message, FILE_APPEND);
